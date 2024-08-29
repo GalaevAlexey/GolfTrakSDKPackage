f72a57284306fc77d14069b06e6a183ef10858a4
Expanse <ioswelltodo@gmail.com>
2024-08-29 15:56:24 +0800

    try opencv package

--- Package.swift
+++ Package.swift
@@ -6,25 +6,17 @@ import PackageDescription
 let package = Package(
     name: "GolfTrakSDKPackage",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v14)
     ],
     products: [
         .library(
             name: "GolfTrakSDKPackage",
-            targets: ["GolfTrakSDKPackage"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/yeatse/opencv-spm/", from: "4.10.0"),
+            targets: ["GolfTrakCV"]),
     ],
     targets: [
-        .target(
-            name: "GolfTrakSDKPackage",
-            dependencies: [
-                .target(name: "MyFramework"),
-                .product(name: "OpenCV", package: "opencv-spm"),
-            ]),
         .binaryTarget(
-            name: "MyFramework",
-            path: "./Sources/MyFramework.xcframework")
+            name: "GolfTrakCV",
+            path: "./Sources/GolfTrakCV.xcframework"
+        )
     ]
 )
--- Sources/GolfTrakSDKPackage/GolfTrakSDKPackage.swift
+++ Sources/GolfTrakSDKPackage/GolfTrakSDKPackage.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import MyFramework
+
 public struct GolfTrakSDKPackage {
     static func loadDependencies() {
         
